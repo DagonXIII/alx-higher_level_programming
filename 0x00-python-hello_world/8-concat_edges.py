093f088bbc131db5141e831c542aff0eddf29b9c
Jese Nicolle <entity01@pm.me>
2023-05-08 03:44:40 -0700

    split for Task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = "object-oriented programming with Python"
+print(str.split()[5][:-1] + "-" + str.split()[6] + " " + str.split()[11][:-1])
 print(str)
