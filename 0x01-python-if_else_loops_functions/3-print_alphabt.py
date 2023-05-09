669db5d4bd3120d09d2672c9c502fa434d4c0fc6
Jese Nicolle <entity01@pm.me>
2023-05-09 03:24:37 -0700

    Task 3 - update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if chr(i) not in ['e', 'q']:
-        print(chr(i), end="")
+for i in range(ord('a'), ord('z')+1):
+    if i != ord('e') and i != ord('q'):
+        print(f"{chr(i)}", end="")
