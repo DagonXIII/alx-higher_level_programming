1d36c2ec269367c9e93b8a7923b96b5e9fe5576b
Jese Nicolle <entity01@pm.me>
2023-05-09 04:25:31 -0700

    Task 14 update 1

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('A') - 1, -1):
-    if i % 2 == 1:
-        print(f"{i:c}", end="")
-    else:
-        print(f"{i:c}".upper(), end="")
+for i in range(122, 64, -1):
+    print("{:c}".format(i if i % 2 == 0 else i - 32), end="")
