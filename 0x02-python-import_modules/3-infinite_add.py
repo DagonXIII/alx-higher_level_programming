481c0b752cdefc734c067500a30700620e60d316
Jese Nicolle <entity01@pm.me>
2023-05-11 04:52:25 -0700

    Task 3 update 0

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-import sys
-
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    if len(args) == 0:
-        print("0")
-    else:
-        sum = 0
-        for arg in args:
-            sum += int(arg)
-        print(sum)
+    import sys
+
+    if __name__ == "__main__":
+        args = sys.argv[1:]
+        if len(args) == 0:
+            print("0")
+        else:
+            sum = 0
+            for arg in args:
+                sum += int(arg)
+            print(sum)
