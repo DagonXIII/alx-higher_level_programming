7b7c57314cf09de9f88bb9faa7a0666cb0a11776
Jese Nicolle <entity01@pm.me>
2023-05-11 04:28:31 -0700

    Task 0 update 4

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+from add_0 import add
+
 a = 1
 b = 2
-import add_0
-print("{0} + {1} = {2}".format(a, b, add(a, b)))
+result = add(a, b)
+
+print("{} + {} = {}".format(a, b, result))
