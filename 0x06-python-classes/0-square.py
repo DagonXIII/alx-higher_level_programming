667bdb4dcde980b1c88f5cc927e0334048b139e2
Jese Nicolle <entity01@pm.me>
2023-05-30 04:35:58 -0700

    Task 0 - update 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
 class Square:
-    """This class represents a square.
-    
-    The __init__ method is defined with the pass phrase indicating
-    that there is no specific code within the method.
     """
-    def __init__(self):
-        pass
+    This class represents a square.
+    """
+    pass
