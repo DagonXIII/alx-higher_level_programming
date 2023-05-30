a532ea05aa81545e093f2a4b190e4949b2588c67
Jese Nicolle <entity01@pm.me>
2023-05-30 04:25:06 -0700

    Mandatory tasks - complete

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,7 +3,6 @@
 class Square:
     """This class represents a square.
     
-    There are no modules or functions in this class.
     The __init__ method is defined with the pass phrase indicating
     that there is no specific code within the method.
     """
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+class Square:
+    """This class represents a square.
+
+    The Square class defines a square with a private instance attribute
+    size. It allows for instantiation with an optional size parameter.
+
+    Args:
+        size (int, optional): The size of the square. Defaults to 0.
+
+    Raises:
+        TypeError: If size is not an integer.
+        ValueError: If size is less than 0.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
