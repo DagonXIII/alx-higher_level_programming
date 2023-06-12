364fa9a5515fcfc811a5000c240f74b6c95e840d
Jese Nicolle <entity01@pm.me>
2023-06-12 07:18:29 -0700

    Task 10 - complete

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module 10-square
+Defines a class Square that inherits from Rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class that represents a square
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a Square instance
+        Args:
+            size (int): The size of the square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square
+        Returns:
+            The area of the square
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        Returns a string representation of the square
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
