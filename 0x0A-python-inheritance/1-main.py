5d7c5b5432bc1969657b62cc0818ef6656801e00
Jese Nicolle <entity01@pm.me>
2023-06-12 05:56:21 -0700

    Task 1 - update 0

--- 0x0A-python-inheritance/1-main.py
+++ 0x0A-python-inheritance/1-main.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 MyList = __import__('1-my_list').MyList
 
 my_list = MyList()
@@ -7,6 +8,8 @@ my_list.append(4)
 my_list.append(2)
 my_list.append(3)
 my_list.append(5)
-print(my_list)
-my_list.print_sorted()
-print(my_list)
+
+with open('tests/1-my_list.txt', 'w') as file:
+    file.write(str(my_list) + '\n')
+    my_list.print_sorted()
+    file.write(str(my_list) + '\n')
