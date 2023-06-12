83ebc966f9a7fae2eb33dc5f5c83572851591e1d
Jese Nicolle <entity01@pm.me>
2023-06-12 07:05:26 -0700

    Task 5 update 0 Task 6 - complete

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -9,4 +9,3 @@ class BaseGeometry:
     Empty class BaseGeometry.
     """
     pass
-
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module base_geometry
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class.
+    """
+
+    def area(self):
+        """
+        Calculates the area of the geometry.
+        """
+        raise Exception("area() is not implemented")
