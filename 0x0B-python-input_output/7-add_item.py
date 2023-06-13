dcc3193b868d36dedde89a335df42df125f0ff86
Jese Nicolle <entity01@pm.me>
2023-06-13 07:34:22 -0700

    Task 7 - update 0

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,16 +12,11 @@ from 6-load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
-# Check if the file exists
 if path.exists(filename):
-    # Load the existing list from the file
     my_list = load_from_json_file(filename)
 else:
-    # Create a new empty list
     my_list = []
 
-# Add the command line arguments to the list
 my_list.extend(sys.argv[1:])
 
-# Save the updated list to the file
 save_to_json_file(my_list, filename)
