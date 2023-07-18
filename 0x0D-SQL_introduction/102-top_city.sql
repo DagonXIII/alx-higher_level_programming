cff7c6a593021e5765421bc133f2576d6f4ba024
Jese Nicolle <entity01@pm.me>
2023-07-18 04:09:13 -0700

    Task 102 - complete

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,8 @@
+-- Calculate the top 3 cities with the highest temperature
+-- during July and August
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+WHERE MONTH(date) IN (7, 8)
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
