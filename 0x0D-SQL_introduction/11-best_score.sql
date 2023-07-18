f7c8ce4ad5043a93cbae3e79d279f2c65d28822e
Jese Nicolle <entity01@pm.me>
2023-07-18 03:55:56 -0700

    Task 11 - complete

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,5 @@
--- List all records with score >= 10 in the table second_table, ordered by score (top first)
+-- List all records with score >= 10 in the table second_table
+-- Ordered by score (top first)
 SELECT score, name
 FROM second_table
 WHERE score >= 10
