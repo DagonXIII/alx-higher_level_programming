bf083a23dc31188e5a787ddcfe032eb06b32a5a9
Jese Nicolle <entity01@pm.me>
2023-07-18 03:56:32 -0700

    Task 11 - complete

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,6 +1,6 @@
--- List all records with score >= 10 in the table second_table
--- Ordered by score (top first)
-SELECT score, name
-FROM second_table
-WHERE score >= 10
-ORDER BY score DESCi;
+-- Lists all records in the table second_table with a score >= 10.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `score` >= 10
+ORDER BY `score` DESC;
